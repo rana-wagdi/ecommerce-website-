2db3903c206e0427b32be89327288901d5dd7680
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-12-03 11:10:58 -0800

    video

--- js/app.js
+++ js/app.js
@@ -8,5 +8,23 @@ $(document).ready(function(){
         $('.fa-hamburger').removeClass('fa-times');
         $('nav').removeClass('active');
 
-    })
+    });
+    $('.dot1').click(function(){
+        $('.vid1').css('display','block');
+        $('.vid2').css('display','none');
+        $('.vid3').css('display','none');
+
+    });
+    $('.dot2').click(function(){
+        $('.vid2').css('display','block');
+        $('.vid1').css('display','none');
+        $('.vid3').css('display','none');
+
+    });
+    $('.dot3').click(function(){
+        $('.vid3').css('display','block');
+        $('.vid2').css('display','none');
+        $('.vid1').css('display','none');
+
+    });
 });
